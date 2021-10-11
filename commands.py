ddc58f093dbcfe7f1403f6105410184913aeb695
Joilton_Alvino <joilton.alvino@genialideias.com>
2021-10-11 09:26:43 -0300

    create e list questions

--- commands.py
+++ commands.py
@@ -1,3 +1,10 @@
+# commans commands
 COMMAND_EXIT = "exit"
-COMMAND_CREATE_PLAYER = "add"
-COMMAND_LIST_PLAYERS = "list"
+
+#players commands
+COMMAND_CREATE_PLAYER = "add -p"
+COMMAND_LIST_PLAYERS = "list -p"
+
+# questions commands
+COMMAND_CREATE_QUESTIONS = "add -q"
+COMMAND_LIST_QUESTIONS = "list -q"
--- play.py
+++ play.py
@@ -1,19 +1,19 @@
-from commands import (
-    COMMAND_EXIT, 
-    COMMAND_CREATE_PLAYER,
-    COMMAND_LIST_PLAYERS
-)
+import commands as cmd
 from settings  import GAME_NAME
-from services import create_player, list_players
+from services import create_player, list_players, create_question, list_questions
 
 run = True
 
 while run:
     commund = input(f"{GAME_NAME} >> ")
 
-    if commund == COMMAND_EXIT:
+    if commund == cmd.COMMAND_EXIT:
          run = False
-    if commund == COMMAND_CREATE_PLAYER:
+    if commund == cmd.COMMAND_CREATE_PLAYER:
         create_player()
-    if commund == COMMAND_LIST_PLAYERS:
+    if commund == cmd.COMMAND_LIST_PLAYERS:
         list_players()
+    if command == cmd.COMMAND_CREATE_QUESTIONS:
+        create_question()
+    if command == cmd.COMMAND_LIST_QUESTIONS:
+        list_questions()
